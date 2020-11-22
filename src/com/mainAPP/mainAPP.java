8af874fa68ec7882d48f1537133b81551df42b7d
merdaalcanto <merdaalcanto@hotmail.com>
2020-11-22 14:28:56 +0100

    Fase 1 done

---
+++ src/com/mainAPP/mainAPP.java
@@ -0,0 +1,73 @@
+package com.mainAPP;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+// M9 exercise classes and methods
+import static com.mainAPP.fase1.*;
+
+public class mainAPP {
+
+	public static void main(String[] args) {
+		
+		// -----     FASE 1     -----
+		System.out.println("-----     FASE 1     -----\n\n");
+		
+		// Ex-1
+		System.out.println("- Exercici 1 -\n");
+		List<String> namesList = Arrays.asList("Ohm", "Ot", "Oscar", "Otilia", "Pepe", "Apu", "apu", "Carlos", "Sonia", "Angel", "Anna", "Ana", "ana", "Sofia", "Anacleto", "Maria", "Alberto", "Alp", "alp");
+		System.out.println("\tNoms -> " + namesList.toString() + "\n");
+		System.out.print("\tResult: " + get3LetterNamesBeginingWithA(namesList));
+		
+		
+		// Ex-2
+		List<Integer> numbersList = Arrays.asList(1, 2 , 3, 10, 55, 123);
+		System.out.println("\n\n- Exercici 2 -\n");
+		System.out.println("\tNumeros -> " + numbersList.toString() + "\n");
+		
+		System.out.println("\tResult: " + identifyParityOfNumbers(numbersList));
+		
+		
+		// Ex-3
+		// We'll use the same List as in Ex-1 -> namesList.
+		System.out.println("\n\n- Exercici 3 -\n");
+		System.out.println("\tNoms -> " + namesList.toString() + "\n");
+		
+		System.out.println("\tResult: " + getStringsWithLetterO(namesList));
+		
+		
+		// Ex-4
+		// We'll use the same List as in Ex-1 -> namesList.
+		System.out.println("\n\n- Exercici 4 -\n");
+		System.out.println("\tNoms -> " + namesList.toString() + "\n");
+		
+		System.out.println("\tResult: " + getStringsWithLetterOLonger5Letters(namesList));
+		
+		
+		// Ex-5
+		List<String> monthsList = Arrays.asList("Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Desembre");
+		System.out.println("\n\n- Exercici 5 -\n");
+		System.out.println("\tMesos -> " + monthsList.toString() + "\n");
+		
+		Consumer<String> print = string -> System.out.print(string + ", ");
+		System.out.print("\tResult: ");
+		monthsList.forEach(print); 
+		
+
+		// Ex-6
+		System.out.println("\n\n- Exercici 6 -\n");
+		System.out.println("\tMesos -> " + monthsList.toString() + "\n");
+		
+		System.out.print("\tResult: ");
+		monthsList.forEach(System.out::print); 
+
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
